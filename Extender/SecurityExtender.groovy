25c1aa90b15c4b4ef5af37a5f8541f072b64c703
Gator-Go <gator@sw-builder.com>
2026-08-19 15:00:53 -0400

    fix extender code

--- Extender/SecurityExtender.groovy
+++ Extender/SecurityExtender.groovy
@@ -1,31 +1,27 @@
+/**
+ * NOTE: The code in this script was created on Windows.
+ * Groovy multiline strings always use \n internally.
+ * We normalize both the file content and the markers to \n before replacing
+ * so the match succeeds whether the Java sources have \n or \r\n.
+ */
+
+class MyData {
+    String theFile
+    String extMarker
+    String srcInsert
+}
+
 def changes = []
 
-public class MyData {
-
-  def theFile = ""
-
-  def extMarker =
-"""
-"""
-  def srcInsert =
-"""
-"""
-}
-MyData newData = new MyData()
-
-
-// ******** Change to delete videos ******** 
-
-newData = new MyData()
-newData.theFile = "MovementService.java"
-newData.extMarker =
-"""
+// ******** Change to delete videos ********
+changes << new MyData(
+    theFile: "MovementService.java",
+    extMarker: """
             movement.setLastUpdate(new Date());
             movement.setDeleteFlag(boolVal);
             em.merge(movement);
-"""
-newData.srcInsert =
-"""
+""",
+    srcInsert: """
             if (movement.getMovementVideoVersion().intValue() > 0) {
                 String videoDir = System.getProperty("com.sw-builder.sync.app.video.dir");
                 String movementVideoHome = videoDir + "Movement/MovementVideo/";
@@ -40,53 +36,43 @@ newData.srcInsert =
             movement.setDeleteFlag(boolVal);
             em.merge(movement);
 """
-changes.add(newData)
-
-
-
+)
 // ******** Done ********
 
-
-def dir = "../security"
-
-def extFiles ( theDir, changes ) {
-
-   def fileList = new File(theDir).list().toList()
-
-   for ( i in fileList ) {
-
-      def inFile = theDir + "/" + i
-      def f1= new File(inFile)
-
-      MyData myData = new MyData();
-
-      if ( f1.isDirectory() ) {
-         extFiles ( inFile, changes )
-      } else {
-//println(i)
-        for (c in changes) {
-          MyData theData = c
-          if ( i.equals(theData.theFile) ) {
-            def oldFile = new File(inFile).text
-            def newMarker = theData.extMarker.replaceAll( "\\\n", "\\\r\\\n" )
-            def newSrc = theData.srcInsert.replaceAll( "\\\n", "\\\r\\\n" )
-            def newFile = oldFile.replace(newMarker, newSrc)
-            new File(inFile).write(newFile)
-            if (newFile.contains(newSrc) == false) { println(theData.theFile + " missing changes") }
-          }
-
-        }
-
-      } 
-   }
+def normalize(String text) {
+    text.replaceAll(/\r\n|\r/, "\n")
 }
 
-extFiles ( dir, changes )
+def processFiles(File dir, List<MyData> changes) {
+    dir.eachFileRecurse { file ->
+        if (!file.isFile()) return
 
+        changes.each { data ->
+            if (file.name == data.theFile) {
+                def oldText = normalize(file.text)
+                def marker  = normalize(data.extMarker)
+                def insert  = normalize(data.srcInsert)
+                def newText = oldText.replace(marker, insert)
 
-def CheckMaxVideosObserver = new File("./CheckMaxVideosObserver.java").text
-new File("../security/security-ejb/src/main/java/com/swBuilder/security/app/event/CheckMaxVideosObserver.java").write(CheckMaxVideosObserver)
+                file.write(newText)
 
-def src = new File("../security_logo.png").newDataInputStream()
-def dst = new File("../security/security-war/src/main/webapp/resources/gfx/logo.png").newDataOutputStream()
-dst << src
+                if (!newText.contains(insert)) {
+                    println "${data.theFile} missing changes"
+                }
+            }
+        }
+    }
+}
+
+processFiles(new File("../security"), changes)
+
+// Copy observer class
+new File("../security/security-ejb/src/main/java/com/swBuilder/security/app/event/CheckMaxVideosObserver.java")
+    .text = new File("./CheckMaxVideosObserver.java").text
+
+// Copy logo
+new File("../security_logo.png").withInputStream { src ->
+    new File("../security/security-war/src/main/webapp/resources/gfx/logo.png").withOutputStream { dst ->
+        dst << src
+    }
+}
